c85ea9decec898bda82b8fa4b5f1897725cea836
tokitaka <artigen3@gmail.com>
2023-06-08 18:49:50 +0900

    1st commit

---
+++ src/main/java/movie_reservation/online_movie_reserv_project/OnlineMovieReservProjectApplication.java
@@ -0,0 +1,13 @@
+package movie_reservation.online_movie_reserv_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMovieReservProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMovieReservProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/movie_reservation/online_movie_reserv_project/OnlineMovieReservProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package movie_reservation.online_movie_reserv_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMovieReservProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
